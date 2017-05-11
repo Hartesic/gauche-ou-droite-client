de7f4d499be8b7688cb8913c38d5cac906bbcd11
Sir_Winn3r <sir.winn3r@gmail.com>
2017-05-11 21:26:21 +0200

    Fix some typos

--- src/components/Quiz/Results.js
+++ src/components/Quiz/Results.js
@@ -34,7 +34,7 @@ class Results extends PureComponent {
         </div>
         <div className="results-text text">Fini !</div>
         <div className="results-text text">Alors, surpris(e) ?</div>
-        <div className="results-text text">Hésite pas à partager notre petit jeu à tes amis, connaissances, parents, animaux de compagnie... tout le monde quoi :)</div>
+        <div className="results-text text">N'hésite pas à partager notre petit jeu à tes amis, connaissances, parents, animaux de compagnie... Tout le monde quoi ! :)</div>
         <div className="results-text text">Ton score :</div>
         <div className="results-score">{this.getScore()} <span>/ {questionCount}</span></div>
         <button className="results-button button-playagain" onClick={e => playAgainFn()}>Recommencer !</button>
