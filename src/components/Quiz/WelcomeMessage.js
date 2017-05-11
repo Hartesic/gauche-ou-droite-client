9b762b912294da4624f0f522c8013753d75ad847
Sir_Winn3r <sir.winn3r@gmail.com>
2017-05-11 20:42:13 +0200

    Fix some texts (again lol)

--- src/components/Quiz/WelcomeMessage.js
+++ src/components/Quiz/WelcomeMessage.js
@@ -5,9 +5,9 @@ const WelcomeMessage = () => (
     <div className="logo">
       <img src="images/logo.svg" />
     </div>
-    <div className="welcome-message-text text">Avez-vous déjà entendu des réflexions de ce genre :</div>
+    <div className="welcome-message-text text">Peut-être avez-vous déjà entendu ce type de réflexions :</div>
     <div className="welcome-message-text text">"Il a bien une tête de gaucho, lui !" ou "Il a pas une dégaine à voter Mitterand..."</div>
-    <div className="welcome-message-text text"><i>Gauche ou Droite ?</i> a pour objectif de tester cette afirmation.</div>
+    <div className="welcome-message-text text"><i>Gauche ou Droite ?</i> a pour objectif de vérifier ces affirmations.</div>
     <div className="welcome-message-text text">Le bord politique d'un député se reconnaît-il au premier coup d'oeil ? Nos amis de l'assemblée sont-ils victimes de délit de faciès ?</div>
     <div className="welcome-message-text text">À vous de nous le dire !</div>
   </div>
