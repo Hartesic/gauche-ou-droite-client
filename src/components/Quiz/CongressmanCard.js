8c49b187f07138fe84f9c34b7bc7b8baca093bfd
Nicolas RIVIERE <nico.riviere78@gmail.com>
2017-05-29 22:41:02 +0200

    add css animations

--- src/components/Quiz/CongressmanCard.js
+++ src/components/Quiz/CongressmanCard.js
@@ -1,11 +1,11 @@
 import React from 'react'
 
-const CongressmanCard = ({ congressman, infosToDisplay=[] }) => (
-  <div className="congressman-container">
-    <div className="congressman-photo">
-      {infosToDisplay.includes('photo') && <img className="congressman-photo" src={congressman.path} alt="Photo d'un député" />}
-    </div>
-    <div className="congressman-infos">
+const CongressmanCard = ({ congressman, infosToDisplay=[], answer }) => (
+  <div className="congressman-container" >
+      <div  className={'congressman-photo animated ' + (infosToDisplay.includes('name') ? (answer == congressman.side  ? 'pulse' : 'shake') : 'fadeInUp')}>
+          {infosToDisplay.includes('photo') && <img className="congressman-photo" src={congressman.path} alt="Photo d'un député" />}
+      </div>
+    <div className="congressman-infos animated fadeInUp">
       {infosToDisplay.includes('name') && <span className="congressman-name">{congressman.name}</span>}
       {infosToDisplay.includes('party') && <span className="congressman-party">{congressman.parti}</span>}
       {infosToDisplay.includes('successRate') && <i className="congressman-successrate">{congressman.average_success * 100}% ont trouvé</i>}
--- src/components/Quiz/Question.js
+++ src/components/Quiz/Question.js
@@ -5,13 +5,13 @@ import QuizProgress from './QuizProgress'
 
 const Question = ({ apiUrl, congressman, currentQuestionIndex, questionCount, selectAnswerFn }) => (
   <div className="question-container">
-    <div className="logo">
+    <div className="logo animated fadeInUp">
       <img src="images/logo.svg" />
     </div>
     <div className="question-choice">
       <CongressmanCard congressman={congressman} infosToDisplay={['photo']} />
-      <button className='question-button button-left' onClick={e => selectAnswerFn('gauche')}>Gauche</button>
-      <button className='question-button button-right' onClick={e => selectAnswerFn('droite')}>Droite</button>
+      <button className='question-button button-left animated fadeInLeftButton' onClick={e => selectAnswerFn('gauche')}>Gauche</button>
+      <button className='question-button button-right animated fadeInRightButton' onClick={e => selectAnswerFn('droite')}>Droite</button>
     </div>
     <QuizProgress currentQuestionIndex={currentQuestionIndex} questionCount={questionCount} />
   </div>
