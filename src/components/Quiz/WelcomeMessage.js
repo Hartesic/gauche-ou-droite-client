c3b0e73fd941995a4085c2fd69b172d0ba77dfbc
Nicolas RIVIERE <nico.riviere78@gmail.com>
2017-06-12 13:29:07 +0200

    detect landscape orientation + mobile fixes

--- src/components/Quiz/WelcomeMessage.js
+++ src/components/Quiz/WelcomeMessage.js
@@ -7,7 +7,7 @@ const WelcomeMessage = () => (
     </div>
     <div className="welcome-message-text text">Peut-être avez-vous déjà entendu ce type de réflexions :</div>
     <div className="welcome-message-text text">"Il a bien une tête de gaucho, lui !" ou "Il a pas une dégaine à voter Mitterand..."</div>
-    <div className="welcome-message-text text"><i>Gauche ou Droite ?</i> a pour objectif de vérifier ces affirmations.</div>
+    <div className="welcome-message-text text hidden-mini-devices"><i>Gauche ou Droite ?</i> a pour objectif de vérifier ces affirmations.</div>
     <div className="welcome-message-text text">Le bord politique d'un député se reconnaît-il au premier coup d'oeil ? Faites le test avant qu'ils ne soient remplacés</div>
     <div className="welcome-message-text text">À vous de nous le dire !</div>
   </div>
